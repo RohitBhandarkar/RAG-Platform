b078d1ab57d1cd1a836054350902a1d901223624
RohitBhandarkar <rohitbhandarkar@hotmail.com>
2026-02-01 14:23:42 +0530

    fix: canonical json commented removed

--- scripts/canonical_json_builder.py
+++ scripts/canonical_json_builder.py
@@ -7,87 +7,84 @@ from pathlib import Path
 
 
 def get_canonical_schema():
-    # return {
-    #     "document": {
-    #         "title": "",
-    #         "authors": [],
-    #         "journal": "",
-    #         "year": None,
-    #         "doi": "",
-    #         "pmid": "",
-    #         "url": "",
-    #         "abstract_summary": "",
-    #         "document_type": "",
-    #         "ingestion_notes": ""
-    #     },
-    #     "api": {
-    #         "name": "",
-    #         "synonyms": [],
-    #         "molecular_weight": None,
-    #         "melting_point_c": None,
-    #         "glass_transition_temp_c": None,
-    #         "logp": None,
-    #         "logd": None,
-    #         "pka": None,
-    #         "bcs_class": "",
-    #         "aqueous_solubility": "",
-    #         "stability_sensitivities": [],
-    #         "dose_mg": None
-    #     },
-    #     "formulation_strategy": {
-    #         "strategy_name": "Nanocrystal",
-    #         "description": "",
-    #         "expected_benefits": [],
-    #         "constraints": []
-    #     },
-    #     "components": [
-    #         {
-    #             "name": "",
-    #             "role": "",
-    #             "amount": {
-    #                 "value": None,
-    #                 "unit": ""
-    #             }
-    #         }
-    #     ],
-    #     "process": {
-    #         "method": "",
-    #         "milling": {
-    #             "time_min": None,
-    #             "bead_size_mm": None,
-    #             "rpm": None
-    #         },
-    #         "solvent_system": "",
-    #         "temperature_c": None,
-    #         "mixing": "",
-    #         "notes": ""
-    #     },
-    #     "particle_characteristics": {
-    #         "particle_size_distribution_nm": "",
-    #         "zeta_potential_mv": None,
-    #         "dissolution_profile": ""
-    #     },
-    #     "stability": {
-    #         "conditions": "",
-    #         "duration_months": None,
-    #         "summary": ""
-    #     },
-    #     "pharmacokinetics": {
-    #         "relative_bioavailability": None,
-    #         "cmax": None,
-    #         "auc": None,
-    #         "tmax": None
-    #     },
-    #     "metadata": {
-    #         "formulation_type": "nanocrystal",
-    #         "data_source": "",
-    #         "confidence_level": "",
-    #         "curation_notes": "",
-    #         "created_at": datetime.now().isoformat()
-    #     }
-    # }
     return {
-
+        "document": {
+            "title": "",
+            "authors": [],
+            "journal": "",
+            "year": None,
+            "doi": "",
+            "pmid": "",
+            "url": "",
+            "abstract_summary": "",
+            "document_type": "",
+            "ingestion_notes": ""
+        },
+        "api": {
+            "name": "",
+            "synonyms": [],
+            "molecular_weight": None,
+            "melting_point_c": None,
+            "glass_transition_temp_c": None,
+            "logp": None,
+            "logd": None,
+            "pka": None,
+            "bcs_class": "",
+            "aqueous_solubility": "",
+            "stability_sensitivities": [],
+            "dose_mg": None
+        },
+        "formulation_strategy": {
+            "strategy_name": "Nanocrystal",
+            "description": "",
+            "expected_benefits": [],
+            "constraints": []
+        },
+        "components": [
+            {
+                "name": "",
+                "role": "",
+                "amount": {
+                    "value": None,
+                    "unit": ""
+                }
+            }
+        ],
+        "process": {
+            "method": "",
+            "milling": {
+                "time_min": None,
+                "bead_size_mm": None,
+                "rpm": None
+            },
+            "solvent_system": "",
+            "temperature_c": None,
+            "mixing": "",
+            "notes": ""
+        },
+        "particle_characteristics": {
+            "particle_size_distribution_nm": "",
+            "zeta_potential_mv": None,
+            "dissolution_profile": ""
+        },
+        "stability": {
+            "conditions": "",
+            "duration_months": None,
+            "summary": ""
+        },
+        "pharmacokinetics": {
+            "relative_bioavailability": None,
+            "cmax": None,
+            "auc": None,
+            "tmax": None
+        },
+        "metadata": {
+            "formulation_type": "nanocrystal",
+            "data_source": "",
+            "confidence_level": "",
+            "curation_notes": "",
+            "created_at": datetime.now().isoformat()
+        }
     }
 
 
